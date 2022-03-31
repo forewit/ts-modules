605d0d8c20862142251da66e971c2b3ff7a11ecd
Marc Anderson <marcda495@gmail.com>
2022-03-31 00:31:31 -0400

    na

--- example/index.ts
+++ example/index.ts
@@ -1,3 +1,14 @@
 import * as Gestures from '../src/modules/gestures.js';
 
 console.log("hi");
+
+
+
+let testDiv = document.getElementsByClassName("test")[0];
+Gestures.track(testDiv);
+testDiv.addEventListener("gesture", function(e: CustomEvent) {
+    console.log(e.detail);
+});
+Gestures.listAll();
+
+console.log("bye");
